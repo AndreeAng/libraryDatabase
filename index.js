985e6dba27a718a8420a6dd60b9e833e47aa4a76
Katie Tooher <44885677+kt3a@users.noreply.github.com>
2022-05-12 13:16:27 -0500

    added button to switch pages, adding a book and audiobook work, displaying of select queries for both book and audio book not quite working yet

--- index.js
+++ index.js
@@ -17,26 +17,102 @@ app.engine('hbs', hbs.engine({
     extname: '.hbs'
 }));
 
-app.use(express.static('public'));
+app.use(express.static('public'));      //uses static style files located in the /public folder
 
-//body-parser module parse incoming request body and store the data in req.body
+//body-parser module parse incoming request body and store the data in req.body json body-parser middleware
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
 app.use(bodyParser.json())
 
+
+app.get('/index.hbs', (req, res) => {
+    let query = "SELECT * FROM library.book";
+    let query_audiobook = "SELECT * FROM library.audiobook";
+    let items = []
+    let auds = []
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        items = result
+        console.log(items)
+        res.render('index', {
+            items: items
+        })
+    })
+    // con.query(query_audiobook, (err, result) => {
+    //     if (err) throw err;
+    //     auds = result
+    //     console.log(auds)
+    //     res.render('index', {
+    //         auds: auds
+    //     })
+    // })
+});
+
+// app.get('/index.hbs', (req, res) => {
+//     let query = "SELECT * FROM library.audiobook";
+//     let auds = []
+//     con.query(query, (err, result) => {
+//         if (err) throw err;
+//         auds = result
+//         console.log(auds)
+//         res.render('index', {
+//             auds: auds
+//         })
+//     })
+// });
+
 app.get('/', (req, res) => {
-    //serves the body of the page to the container
-    res.render('index', {
+    res.render('index',{
+        items: []
+    })
+})
+
+app.get('/customer.hbs', (req,res) => {
+    res.render('customer', {
         items: []
     });
 });
 
+
+// app.get('/', (req, res) => {
+//     //serves the body of the page to the container
+//     let query = "SELECT * FROM library.book";
+//     let items = [];
+//     con.query(query, (err, result) => {
+//         if (err) throw err;
+//         items = result
+//         console.log(items)
+//         res.redirect('/customer')
+//     })
+// });
+
 app.post('/', (req, res) => {
-    console.log(req.body)
-    res.redirect('/')
+    let query = "INSERT INTO library.book(ibsn, name, status, author, genre) VALUES ?;";
+    data = [
+        [req.body.ibsn, req.body.name, req.body.status, req.body.author, req.body.genre]
+    ]
+    con.query(query, [data], (err, result) => {
+        if (err) throw err;
+        console.log(result)
+        res.redirect('/')
+    })
 });
+
+
+app.post('/audiobook', (req, res) => {
+    let query = "INSERT INTO library.audiobook(id, name, status, length, author, narrator, genre) VALUES ?;";
+    data = [
+        [req.body.id, req.body.name, req.body.status, req.body.length, req.body.author, req.body.narrator, req.body.genre]
+    ]
+    con.query(query, [data], (err, result) => {
+        if (err) throw err;
+        console.log(result)
+        res.redirect('/')
+    })
+});
+
 
 // port where app is served (port 3000)
 app.listen(3000, () => {
--- models/taskModel.js
+++ models/taskModel.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "PUT YOUR PASSWORD HERE"
+    password: "PUT PASSWORD HERE"
 });
 
 con.connect(function (err) {
